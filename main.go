6382ddb089966ab89382bbf726a23eff7766924d
liubin <32267944+liubinpy@users.noreply.github.com>
2025-05-23 15:52:35 +0800

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 
     r.GET("/", func(c *gin.Context) {
         c.JSON(200, gin.H{
-            "message": "Hello, Azure devops!",
+            "message": "Hello, Azure AKS!",
         })
     })
 
